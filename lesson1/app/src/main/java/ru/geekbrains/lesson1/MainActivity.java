0c6461c21bada7472909ce2ede79faf5225bc73c
PavelBelov <gendalfqwe29@gmail.com>
2021-02-20 16:38:47 +0300

    Выполнил задание

--- lesson1/app/src/main/java/ru/geekbrains/lesson1/MainActivity.java
+++ lesson1/app/src/main/java/ru/geekbrains/lesson1/MainActivity.java
@@ -2,7 +2,9 @@ package ru.geekbrains.lesson1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Обработчик события перехода на другой Activity
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(view -> openTwoActivity());
+    }
+
+    public void openTwoActivity(){
+        Intent intent = new Intent(this, TwoActivity.class);
+        startActivity(intent);
     }
 }
